4257a79ed4d8be39033e4101e8d84c52f1f5ca5b
acalcutt <acalcutt@techidiots.net>
2026-02-22 21:33:38 -0500

    Import map UI and MapLibre conversion files from google-maps-removal (map features only)

---
+++ wiglewifiwardriving/src/main/java/net/wigle/wigleandroid/MapTypes.java
@@ -0,0 +1,14 @@
+package net.wigle.wigleandroid;
+
+/**
+ * Compatibility constants replacing GoogleMap map type enums.
+ */
+public final class MapTypes {
+    public static final int MAP_TYPE_NONE = 0;
+    public static final int MAP_TYPE_NORMAL = 1;
+    public static final int MAP_TYPE_SATELLITE = 2;
+    public static final int MAP_TYPE_TERRAIN = 3;
+    public static final int MAP_TYPE_HYBRID = 4;
+
+    private MapTypes() {}
+}
